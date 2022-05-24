d17ba88ca7f0936ef0167eb1a650b753a4800565
Matt Weale <51047506+mattweale@users.noreply.github.com>
2022-05-24 17:47:30 +0100

    Create mount_drive.sh

---
+++ mount_drive.sh
@@ -0,0 +1,9 @@
+lsblk -o NAME,HCTL,SIZE,MOUNTPOINT | grep -i "sd"
+sudo parted /dev/sdb --script mklabel gpt mkpart xfspart xfs 0% 100%  # making partions, do not run on re-attaching an existing drive
+sudo mkfs.xfs /dev/sdb1
+sudo partprobe /dev/sdb1 
+sudo mkdir /datadrive # end making partions
+sudo mount /dev/sdb1 /datadrive #not permanent use fstab
+sudo yum -y install util-linux
+sudo fstrim /datadrive
+sudo chown -R `whoami` /datadrive
