2be98d88cbb017ea0c72b730e900750e8a8f86c8
Matt Weale <51047506+mattweale@users.noreply.github.com>
2022-05-26 15:50:06 +0100

    Update rtstps_install.sh

--- rtstps_install.sh
+++ rtstps_install.sh
@@ -18,68 +18,60 @@ else
 	export NOW=$(date '+%Y%m%d-%H:%M:%S')
 	echo "$NOW	RT-STPS Prerequisites"
 	
-	##sudo yum upgrade -y 
-	#sudo yum install -y java-11-openjdk-devel
-	sudo apt-get install openjdk-11-jdk -y
-
-
-
-	#   Set JAVA Environment Variables
+#	Install JDK
+	sudo yum upgrade -y 
+	sudo yum install -y java-11-openjdk-devel
+	
+#   Set JAVA Environment Variables
 	export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 	export PATH=$PATH:$JAVA_HOME/bin
 
-	#   Download RT_STPS Software and Test Data
+#   Download RT_STPS Software and Test Data
 	export CONTAINER='https://samrw.blob.core.windows.net/sharing/nasa_drl/RT-STPS/'
 	export SAS_TOKEN='?sp=r&st=2022-03-29T15:53:35Z&se=2023-03-29T23:53:35Z&spr=https&sv=2020-08-04&sr=c&sig=lxDbvzZCZ2DUkbrFEw%2B1nXPegTB9IMe5NDFDu1kmlMs%3D'
 
-	sudo azcopy cp "${CONTAINER}RT-STPS_6.0.tar.gz${SAS_TOKEN}" "./"
-	sudo azcopy cp "${CONTAINER}RT-STPS_6.0_PATCH_1.tar.gz${SAS_TOKEN}" "./"
-	sudo azcopy cp "${CONTAINER}RT-STPS_6.0_PATCH_2.tar.gz${SAS_TOKEN}" "./"
-	sudo azcopy cp "${CONTAINER}RT-STPS_6.0_PATCH_3.tar.gz${SAS_TOKEN}" "./"
-	sudo azcopy cp "${CONTAINER}RT-STPS_6.0_testdata.tar.gz${SAS_TOKEN}" "./"
+	export SOURCE_DIR=/datadrive
 	
-	cd ~
-	mkdir data
-	cd data
-	sudo azcopy cp "${CONTAINER}test2.bin${SAS_TOKEN}" "./"
+	sudo azcopy cp "${CONTAINER}RT-STPS_6.0.tar.gz${SAS_TOKEN}" "$RTSTPS_DIR"
+	sudo azcopy cp "${CONTAINER}RT-STPS_6.0_PATCH_1.tar.gz${SAS_TOKEN}" "$RTSTPS_DIR"
+	sudo azcopy cp "${CONTAINER}RT-STPS_6.0_PATCH_2.tar.gz${SAS_TOKEN}" "$RTSTPS_DIR"
+	sudo azcopy cp "${CONTAINER}RT-STPS_6.0_PATCH_3.tar.gz${SAS_TOKEN}" "$RTSTPS_DIR"
+	sudo azcopy cp "${CONTAINER}RT-STPS_6.0_testdata.tar.gz${SAS_TOKEN}" "$RTSTPS_DIR"
 
-	#   Install RT_STPS Software  
-	export NOW=$(date '+%Y%m%d-%H:%M:%S')
-	echo "$NOW	Installing RT-STPS..."
-	cd ~
-	# Extract main package
-	tar xzf RT-STPS_6.0.tar.gz
-	# Apply patch 2 - This patch supersedes Patch 1
-	cp ~/rt-stps/config/jpss1.xml ~/rt-stps/config/jpss1.xml.old
-	tar xzf RT-STPS_6.0_PATCH_2.tar.gz
-	# Apply patch 3
-	tar xzf RT-STPS_6.0_PATCH_3.tar.gz
-	# Extract test data
-	tar xzf RT-STPS_6.0_testdata.tar.gz
-	# Install
-	cd rt-stps
+#	Could you this but need to tidy up Container
+#	azcopy $RTSTPS_SOURCE $RTSTPS_DIR --recursive --overwrite --log-level=error
+
+# 	Install RT-STPS
+	cd $SOURCE_DIR
+	tar -xzvf RT-STPS_6.0.tar.gz
+	cd rt-stps/
+	./install.sh
+# 	Install patches
+	cd $SOURCE_DIR
+	tar -xzvf RT-STPS_6.0_PATCH_1.tar.gz 
+	tar -xzvf RT-STPS_6.0_PATCH_2.tar.gz 
+	tar -xzvf RT-STPS_6.0_PATCH_3.tar.gz
+	cd rt-stps/
 	./install.sh
 
-	# Update leadsec file
+
+# 	Update leadsec file
 	cd ~
 	cd ~/rt-stps
 	./bin/internal/update_leapsec.sh
 
 fi
 
-# Install XRDP Server
-sudo yum groupinstall -y "Server with GUI"
-sudo yum groupinstall -y "Xfce"
-sudo yum install -y tigervnc-server xrdp
-sudo systemctl enable xrdp.service
-sudo systemctl start xrdp.service
-sudo systemctl set-default graphical.target
+# 	Install XRDP Server
 
-# Echo how to start RT-STPS, Viewer and Sender
-echo 'Start RT-STPS Server with: ./rt-stps/jsw/bin/rt-stps-server.sh start'
-echo 'Start Viewer with: ./rt-stps/bin/viewer.sh &'
-echo 'Start Sender with: ./rt-stps/bin/sender.sh &'
+	sudo yum groupinstall -y "Server with GUI"
+	sudo yum groupinstall -y "Xfce"
+	sudo yum install -y tigervnc-server xrdp	
+	sudo systemctl enable xrdp.service
+	sudo systemctl start xrdp.service
+	sudo systemctl set-default graphical.target
 
-# Shutdown
-echo "Finished. Shutting down"
-shutdown -h now
+# 	Echo how to start RT-STPS, Viewer and Sender
+	echo 'Start RT-STPS Server with: ./rt-stps/jsw/bin/rt-stps-server.sh start'
+	echo 'Start Viewer with: ./rt-stps/bin/viewer.sh &'
+	echo 'Start Sender with: ./rt-stps/bin/sender.sh &'
