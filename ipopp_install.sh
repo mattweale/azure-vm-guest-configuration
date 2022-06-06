8e0ecee87e961ee3bcc9142f03eb70d9eed9a8c3
Matt Weale <51047506+mattweale@users.noreply.github.com>
2022-06-06 13:27:38 +0100

    Update ipopp_install.sh

--- ipopp_install.sh
+++ ipopp_install.sh
@@ -20,25 +20,26 @@ else
 #echo 'Enter SAS Token for Container:'
 #read SAS_TOKEN
 
-# Update OS
+#	 Update OS
 sudo yum upgrade -y 
 
-# Install az copy
+#   Install az copy
 cd ~
 curl "https://azcopyvnext.azureedge.net/release20220315/azcopy_linux_amd64_10.14.1.tar.gz" > azcopy_linux_amd64_10.14.1.tar.gz
 tar -xvf azcopy_linux_amd64_10.14.1.tar.gz
-sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
-rm azcopy_linux_amd64_10.14.1.tar.gz
-rm -r ./azcopy_linux_amd64_*/azcopy
+cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
 
-# Install XRDP Server
-sudo yum install -y epel-release
-sudo yum groupinstall -y "Server with GUI"
-sudo yum groupinstall -y "Gnome Desktop"
-sudo yum install -y tigervnc-server xrdp	
-sudo systemctl enable xrdp.service
-sudo systemctl start xrdp.service
-sudo systemctl set-default graphical.target
+#	Apply Udates
+	sudo yum upgrade -y 
+	
+# 	Install XRDP Server
+	sudo yum install -y epel-release
+	sudo yum groupinstall -y "Server with GUI"
+	sudo yum groupinstall -y "Gnome Desktop"
+	sudo yum install -y tigervnc-server xrdp	
+	sudo systemctl enable xrdp.service
+	sudo systemctl start xrdp.service
+	sudo systemctl set-default graphical.target
 
 # Ensure mount_drive.sh is run first
 #sudo mkdir -p /nfsdata
@@ -57,15 +58,15 @@ export CONTAINER='https://samrw.blob.core.windows.net/ipopp/'
 export SAS_TOKEN='sp=racwl&st=2022-04-28T12:33:33Z&se=2023-04-28T20:33:33Z&spr=https&sv=2020-08-04&sr=c&sig=ir5zUR9o7QtijQJqu54iekLJDxNWNRkB2rAgqapT%2FRQ%3D'
 
 echo 'Downloading DRL-IPOPP_4.1.tar.gz.......'
-sudo azcopy cp "${CONTAINER}/${IPOPP_TAR_GZ_FILENAME}?${SAS_TOKEN}" "./${IPOPP_TAR_GZ_FILENAME}"
+azcopy cp "${CONTAINER}/${IPOPP_TAR_GZ_FILENAME}?${SAS_TOKEN}" "./${IPOPP_TAR_GZ_FILENAME}"
 echo 'Downloading DRL-IPOPP_4.1_PATCH_1.tar.gz.......'		
-sudo azcopy cp "${CONTAINER}/${PATCH_FILE_NAME}?${SAS_TOKEN}" "./${PATCH_FILE_NAME}"
+azcopy cp "${CONTAINER}/${PATCH_FILE_NAME}?${SAS_TOKEN}" "./${PATCH_FILE_NAME}"
 
 # Install ipopp
 export HOME=/datadrive
-sudo tar -C $INSTALL_DIR -xzf $IPOPP_TAR_GZ_FILENAME
-sudo chmod -R 755 IPOPP
-sudo /datadrive/IPOPP/install_ipopp.sh -installdir $INSTALL_DIR/drl -datadir $INSTALL_DIR/data  -ingestdir $INSTALL_DIR/data/ingest
+cd $INSTALL_DIR
+su -c 'tar -C $INSTALL_DIR -xzf $IPOPP_TAR_GZ_FILENAME' adminuser
+./IPOPP/install_ipopp.sh -installdir $INSTALL_DIR/drl -datadir $INSTALL_DIR/data  -ingestdir $INSTALL_DIR/data/ingest
 
 # Add SQL Path for Patch Installation DB Check
 export PATH=$PATH:/home/adminuser/drl/standalone/mariadb-10.1.8-linux-x86_64/bin:/home/adminuser/drl/standalone/jdk1.8.0_45/bin
@@ -74,7 +75,7 @@ export PATH=$PATH:/home/adminuser/drl/standalone/mariadb-10.1.8-linux-x86_64/bin
 sudo $INSTALL_DIR/drl/tools/install_patch.sh $PATCH_FILE_NAME
 
 # Start Services
-./$INSTALL_DIR/drl/tools/services.sh start
+/${INSTALL_DIR}/drl/tools/services.sh start
 
 # Create .netrc file for MODIS Sensor geolocation module requires access to additional ephemeris and attitude ancillary files during processing. These files will be automatically retrieved using EarthData portal login credentials.
 
