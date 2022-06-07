feb6d55776da571b9f1488c46d192ed477c4a2c8
Matt Weale <51047506+mattweale@users.noreply.github.com>
2022-06-07 17:34:31 +0100

    Update ipopp_install.sh

--- ipopp_install.sh
+++ ipopp_install.sh
@@ -50,6 +50,9 @@ else
 	export CONTAINER='https://samrw.blob.core.windows.net/ipopp/'
 	export SAS_TOKEN='?sp=rl&st=2022-06-06T18:11:57Z&se=2023-06-07T02:11:57Z&spr=https&sv=2021-06-08&sr=c&sig=xPb9nAWP8Om2ony57uySwlfsmWxNCO7boKEtWYC8qqs%3D'
 	export SOURCE_DIR=/datadrive
+	export INSTALL_DIR=/datadrive
+	export IPOPP_TAR_GZ_FILENAME='DRL-IPOPP_4.1.tar.gz'
+	export PATCH_FILE_NAME='DRL-IPOPP_4.1_PATCH_1.tar.gz'
 
 	azcopy cp "${CONTAINER}DRL-IPOPP_4.1.tar.gz${SAS_TOKEN}" "$SOURCE_DIR"
 	azcopy cp "${CONTAINER}DRL-IPOPP_4.1_PATCH_1.tar.gz${SAS_TOKEN}" "$SOURCE_DIR"
@@ -58,27 +61,28 @@ else
 #	azcopy $RTSTPS_SOURCE $RTSTPS_DIR --recursive --overwrite --log-level=error
 
 # 	Install ipopp
-	cd $HOME
+	cd $SOURCE_DIR
 	tar -C $INSTALL_DIR -xzf $IPOPP_TAR_GZ_FILENAME
 	chmod -R 755 IPOPP
 	./IPOPP/install_ipopp.sh -installdir $INSTALL_DIR/drl -datadir $INSTALL_DIR/data  -ingestdir $INSTALL_DIR/data/ingest
 
 # 	Add SQL Path for Patch Installation DB Check
-	export PATH=$PATH:/home/adminuser/drl/standalone/mariadb-10.1.8-linux-x86_64/bin:/home/adminuser/drl/standalone/jdk1.8.0_45/bin
+#	export PATH=$PATH:/home/adminuser/drl/standalone/mariadb-10.1.8-linux-x86_64/bin:/home/adminuser/drl/standalone/jdk1.8.0_45/bin
+	export PATH=$PATH:/datadrive/ipopp/drl/standalone/mariadb-10.1.8-linux-x86_64/bin:/datadrive/ipopp/drl/standalone/jdk1.8.0_45/bin
 
-# 	Install IPOPP Patch
+# Install IPOPP Patch
 	sudo $INSTALL_DIR/drl/tools/install_patch.sh $PATCH_FILE_NAME
 
-# 	Start Services
-	/${INSTALL_DIR}/drl/tools/services.sh start
+# Start Services
+	${INSTALL_DIR}/drl/tools/services.sh start
 
-# 	Create .netrc file for MODIS Sensor geolocation module requires access to additional ephemeris and attitude ancillary files during processing. These files will be automatically retrieved using EarthData portal login credentials.
+# Create .netrc file for MODIS Sensor geolocation module requires access to additional ephemeris and attitude ancillary files during processing. These files will be automatically retrieved using EarthData portal login credentials.
 
-## 	Verify
-	./$INSTALL_DIR/drl/tools/services.sh status > status.log
-	./$INSTALL_DIR/drl/tools/spa_services.sh status >> status.log
+## Verify
+	/$INSTALL_DIR/drl/tools/services.sh status > status.log
+	/$INSTALL_DIR/drl/tools/spa_services.sh status >> status.log
 
-# 	cp $HOME/drl/SPA/modisl1db/algorithm/DRLshellscripts/sample.netrc $HOME/.netrc
-# 	Next edit the $HOME/.netrc file to replace “yourlogin” and “yourpassword” with your EarthData portal credentials.
+# cp $HOME/drl/SPA/modisl1db/algorithm/DRLshellscripts/sample.netrc $HOME/.netrc
+# Next edit the $HOME/.netrc file to replace “yourlogin” and “yourpassword” with your EarthData portal credentials.
 
 fi
