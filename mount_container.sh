386aa468771b35d52adada370cb5b97e06721cdd
Matt Weale <51047506+mattweale@users.noreply.github.com>
2022-06-07 12:27:37 +0100

    Update mount_container.sh

--- mount_container.sh
+++ mount_container.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #	=============================
 #	NFS Mount Container
@@ -6,14 +6,34 @@
 
 #	Check Linux Distro for nfs utils install
     source /etc/os-release
-    if [echo $NAME="CentOS Linux" ]; then
+    if [ "$NAME" = "CentOS Linux" ]; then
 	    echo "Found RHEL Distro installing using yum"
         sudo yum install nfs-utils
-    else [echo $NAME="Ubuntu"]; then
+    elif [ "$NAME" = "Ubuntu" ]; then
 	    echo "echo Found Ubuntu Distro installing using apt"
         sudo apt install nfs-common
+    else
+        echo "What OS are you running!?"
     fi
 
+#   Check VM Role to mount right Container
+
     sudo mkdir -p /nfsdata
-    sudo mount -o sec=sys,vers=3,nolock,proto=tcp saorbital.blob.core.windows.net:/saorbital/data  /nfsdata
-    sudo chmod -R 777 /nfsdata
+
+    if [ "$HOSTNAME" = "vm-orbital-data-collection" ]; then
+	    echo "Found Data Collection VM mounting /saorbital/raw-data"
+        sudo mount -o sec=sys,vers=3,nolock,proto=tcp saorbital.blob.core.windows.net:/saorbital/raw-data  /nfsdata
+    elif [ "$HOSTNAME" = "vm-orbital-rtstps" ]; then
+	    echo "Found Data Collection VM mounting /saorbital/rt-stps"
+        sudo mount -o sec=sys,vers=3,nolock,proto=tcp saorbital.blob.core.windows.net:/saorbital/rt-stps  /nfsdata
+    elif [ "$HOSTNAME" = "vm-orbital-ipopp" ]; then
+	    echo "Found Data Collection VM mounting /saorbital/ipopp"
+        sudo mount -o sec=sys,vers=3,nolock,proto=tcp saorbital.blob.core.windows.net:/saorbital/ipopp  /nfsdata
+    else
+    #    echo "What VM is this!?"
+    fi
+
+sudo chmod -R 777 /nfsdata
+
+    #    [ "$NAME" = "Ubuntu" ]; echo $?
+    #    [ "$NAME" = "CentOS Linux" ]; echo $?
