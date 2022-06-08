1b425cd1f05347b1000e8bbb16385930da3fa956
Matt Weale <51047506+mattweale@users.noreply.github.com>
2022-06-08 06:45:05 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo ./ubuntu_update.sh
-sudo ./mount_data_drive.sh
-sudo ./mount_container.sh
+source sudo ./ubuntu_update.sh
+source sudo ./mount_data_drive.sh
+source sudo ./mount_container.sh
