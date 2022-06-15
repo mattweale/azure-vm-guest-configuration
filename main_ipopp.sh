788bc52ee1181be8a7883f39a40ddbe3326d9411
Matt Weale <51047506+mattweale@users.noreply.github.com>
2022-06-15 16:38:53 +0100

    Update main_ipopp.sh

--- main_ipopp.sh
+++ main_ipopp.sh
@@ -3,4 +3,4 @@
 sudo ./mount_data_drive.sh
 sudo ./mount_container.sh
 sudo ./install_ipopp_prereqs.sh
-su -c "./install_ipopp_prepare.sh" -s /bin/bash adminuser
+su -c "/datadrive/install_ipopp_prepare.sh" -s /bin/bash adminuser
