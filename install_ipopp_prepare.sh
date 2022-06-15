2f7dbd0b8e6023960a27c569234a9a9302af86b1
Matt Weale <51047506+mattweale@users.noreply.github.com>
2022-06-15 23:10:18 +0100

    Update install_ipopp_prepare.sh

--- install_ipopp_prepare.sh
+++ install_ipopp_prepare.sh
@@ -4,7 +4,7 @@
 # IPOPP Install
 # =============================
 
-#   Set some environment variables
+#   	Set some environment variables
 	export SOURCE_DIR=/datadrive
 	export INSTALL_DIR=/datadrive/IPOPP
 	export IPOPP_TAR_GZ_FILENAME='DRL-IPOPP_4.1.tar.gz'
@@ -24,17 +24,17 @@
 	cp $SOURCE_DIR/DRL-IPOPP_4.1_PATCH_1.tar.gz $INSTALL_DIR/drl
      	$INSTALL_DIR/drl/tools/install_patch.sh DRL-IPOPP_4.1_PATCH_1.tar.gz
 
-# Install IPOPP Patch #2
+# 	Install IPOPP Patch #2
 	cp $SOURCE_DIR/DRL-IPOPP_4.1_PATCH_2.tar.gz $INSTALL_DIR/drl
 	$INSTALL_DIR/drl/tools/install_patch.sh DRL-IPOPP_4.1_PATCH_2.tar.gz
 
-# Start Services
+# 	Start Services
 	$INSTALL_DIR/drl/tools/services.sh start
 
-# Create .netrc file for MODIS Sensor geolocation module requires access to additional ephemeris and attitude ancillary files during processing. These files will be automatically retrieved using EarthData portal login credentials.
-# cp $HOME/drl/SPA/modisl1db/algorithm/DRLshellscripts/sample.netrc $HOME/.netrc
-# Next edit the $HOME/.netrc file to replace “yourlogin” and “yourpassword” with your EarthData portal credentials.
+# 	Create .netrc file for MODIS Sensor geolocation module requires access to additional ephemeris and attitude ancillary files during processing. These files will be automatically retrieved using EarthData portal login credentials.
+	cp $INSTALL_DIR/drl/SPA/modisl1db/algorithm/DRLshellscripts/sample.netrc $HOME/.netrc
+# 	Next edit the $HOME/.netrc file to replace “yourlogin” and “yourpassword” with your EarthData portal credentials.
 
-## Verify
-#	/$INSTALL_DIR/drl/tools/services.sh status > status.log
-#	/$INSTALL_DIR/drl/tools/spa_services.sh status >> status.log
+# 	Verify
+	$INSTALL_DIR/drl/tools/services.sh status > status.log
+	$INSTALL_DIR/drl/tools/spa_services.sh status >> status.log
